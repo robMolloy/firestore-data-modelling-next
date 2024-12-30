8426ed01147e49365b6cdbb2ddf2f51039f7a913
robmolloy <robmolloy@hotmail.co.uk>
2024-12-30 18:07:19 +0000

    cleanup create userTodos tests/mocks

--- firestore/firestoreTestMocks.ts
+++ firestore/firestoreTestMocks.ts
@@ -1,10 +1,20 @@
 import { Timestamp } from "firebase/firestore";
+import { z } from "zod";
 
 export const collectionNames = {
   userTodos: "userTodos",
 };
 
-export const userTodo1 = {
+export const userTodoSchema = z.object({
+  id: z.string(),
+  uid: z.string(),
+  task: z.string(),
+  completed: z.boolean(),
+  createdAt: z.object({ seconds: z.number(), nanoseconds: z.number() }),
+  updatedAt: z.object({ seconds: z.number(), nanoseconds: z.number() }),
+});
+
+export const userTodo1: z.infer<typeof userTodoSchema> = {
   id: "id1",
   uid: "uid1",
   task: "uid1",
